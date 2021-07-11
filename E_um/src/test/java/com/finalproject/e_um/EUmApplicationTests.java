2a7a304df80849d63f9489da74eccaa5f092124d
newkayak12 <newkayak12@gmail.com>
2021-07-11 22:31:37 +0900

    final start

---
+++ E_um/src/test/java/com/finalproject/e_um/EUmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalproject.e_um;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EUmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
