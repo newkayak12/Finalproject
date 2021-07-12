eba55b259fab1f28037d21ddd3240e742019e342
newkayak12 <newkayak12@gmail.com>
2021-07-12 22:56:32 +0900

    eum

---
+++ 21AM_E_um_final/src/test/java/com/AM_E_um_final/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AM_E_um_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
