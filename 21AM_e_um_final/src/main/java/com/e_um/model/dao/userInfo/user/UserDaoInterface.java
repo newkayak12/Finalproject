160bbe9789ae2e79eff5e6d909a418a9577aa0be
newkayak12 <newkayak12@gmail.com>
2021-07-21 23:45:01 +0900

    hambuger fin carousel fin

--- 21AM_e_um_final/src/main/java/com/e_um/model/dao/userInfo/user/UserDaoInterface.java
+++ 21AM_e_um_final/src/main/java/com/e_um/model/dao/userInfo/user/UserDaoInterface.java
@@ -1,5 +1,7 @@
 package com.e_um.model.dao.userInfo.user;
 
+import java.util.List;
+
 import org.mybatis.spring.SqlSessionTemplate;
 
 import com.e_um.model.vo.userInfo.user.User;
@@ -13,5 +15,17 @@ public interface UserDaoInterface {
 	public User login(User user, SqlSessionTemplate session);
 	public User verify(User user, SqlSessionTemplate session);
 	public int changepw(User user, SqlSessionTemplate session);
+	
+	public int insertalarm1(User user, SqlSessionTemplate session);
+	public int insertalarm2(User user, SqlSessionTemplate session);
+	public int insertalarm3(User user, SqlSessionTemplate session);
+	public int insertalarm4(User user, SqlSessionTemplate session);
+	public int insertalarm5(User user, SqlSessionTemplate session);
+	public int insertalarm6(User user, SqlSessionTemplate session);
+	public int insertalarm7(User user, SqlSessionTemplate session);
+	public int insertalarm8(User user, SqlSessionTemplate session);
+	public int insertalarm9(User user, SqlSessionTemplate session);
+	
+	public List<User> recommandFriend(SqlSessionTemplate session);
 	
 }	
